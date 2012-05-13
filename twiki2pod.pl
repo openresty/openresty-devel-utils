620f50160cf6255960257cb0849850c16d89907e
agentzh (章亦春) <agentzh@gmail.com>
2012-05-13 14:42:37 +0800

    checked in scripts for change log generation.

---
+++ twiki2pod.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $level = 0;
+while (<>) {
+    my $new_level;
+    if (s/^(\*{1,})\s+//) {
+        $new_level = length $1;
+
+        if ($new_level > $level) {
+            while ($new_level > $level) {
+                print "\n\n=over\n\n";
+                $level++;
+            }
+
+        } elsif ($new_level == $level) {
+            # do nothing
+
+        } else {
+            # $new_level < $level
+            while ($new_level < $level) {
+                print "\n\n=back\n\n";
+                $level--;
+            }
+        }
+
+        print "\n\n=item *\n\n";
+    }
+
+    s/\[\[([^]]+)\|[^]]+\]\]/$1/g;
+    s/ ~(\w+)/ $1/g;
+    s{http://\S+}{L<$&>}g;
+    s/\{\{\{([^}]+)\}\}\}/C<<< $1 >>>/g;
+
+    print;
+}
+
+while ($level > 0) {
+    print "\n\n=back\n\n";
+    $level--;
+}
+
